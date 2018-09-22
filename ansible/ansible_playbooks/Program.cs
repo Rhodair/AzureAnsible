be8dc88edb2e91ebe3375bc411796ed679155054
David Rachwalik <david.rachwalik@outlook.com>
2018-09-22 00:39:59 -0500

    Initial commit

---
+++ ansible/ansible_playbooks/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ansible_playbooks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
